1b724b2bb86338a72ed79b698165d36c77132e50
JuanCarlosDxT <1juancarlosdt.88@gmail.com>
2025-08-08 21:20:16 -0500

    Update Gas.java
    Se añadió una linea de codigo.

--- Gas/src/gas/Gas.java
+++ Gas/src/gas/Gas.java
@@ -4,6 +4,7 @@ public class Gas {
 
     public static void main(String[] args) {
         System.out.println("Bienvenido al sistema de Venta de Gas - Rapidito");
+        System.out.println("Por favor ingrese su nombre de usuario");
     }
     
 }
